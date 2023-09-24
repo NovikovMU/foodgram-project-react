bedfbcf7dabf42cb84217326af081848677a23b7
NovikovMU <maksnovikov56@gmail.com>
2023-09-25 00:19:37 +0300

    check actions.

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -6,5 +6,6 @@ from .models import Follow, User
 class UsersAdmin(admin.ModelAdmin):
     list_filter = ('email', 'first_name')
 
+
 admin.site.register(User, UsersAdmin)
 admin.site.register(Follow)

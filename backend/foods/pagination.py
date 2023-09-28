653aa25af2a7ad5e933de69cf22f95fb5de2597a
NovikovMU <maksnovikov56@gmail.com>
2023-09-28 15:54:08 +0300

    Diplom is ready.

--- backend/foods/pagination.py
+++ backend/foods/pagination.py
@@ -5,6 +5,6 @@ from .constants import MAX_PAGE_SIZE, PAGE_SIZE
 
 class CommonResultPagination(PageNumberPagination):
     limit = 6
-    page_size = PAGE_SIZE  
+    page_size = PAGE_SIZE
     page_size_query_param = 'limit'
     max_page_size = MAX_PAGE_SIZE
